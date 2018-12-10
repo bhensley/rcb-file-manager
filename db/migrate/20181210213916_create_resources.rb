a787079fa6fa1f4a06fee4d3d132a94c7223912b
Bob Hensley <bhensley@rcbrayshaw.com>
2018-12-10 16:45:43 -0500

    Basic migrations setup.

---
+++ db/migrate/20181210213916_create_resources.rb
@@ -0,0 +1,12 @@
+class CreateResources < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resources do |t|
+      t.string :bucket_name
+      t.string :bucket_region
+      t.string :identity_pool_id
+      t.string :album_path
+
+      t.timestamps
+    end
+  end
+end

ac40ecfbeabb5dcf188e2b4a5108b1d03ce67c54
Bob Hensley <bhensley@rcbrayshaw.com>
2018-12-17 12:33:28 -0500

    Secret key base stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Secret Key Base
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
